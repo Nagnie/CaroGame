e58977176677874327d2ca3b2e2a348dbe7c28d4
Nagnie <tranthaongan7epct@gmail.com>
2024-02-13 22:26:20 +0700

    luv

--- Caro/Control.h
+++ Caro/Control.h
@@ -1,9 +1,3 @@
 #pragma once
 
 void GotoXY(int column, int line);
-int whereX();
-int whereY();
-void MoveRight(_POINT _A[B_SIZE][B_SIZE], int& _X, int& _Y, int& cX, int& cY);
-void MoveLeft(_POINT _A[B_SIZE][B_SIZE], int& _X, int& _Y, int& cX, int& cY);
-void MoveUp(_POINT _A[B_SIZE][B_SIZE], int& _X, int& _Y, int& cX, int& cY);
-void MoveDown(_POINT _A[B_SIZE][B_SIZE], int& _X, int& _Y, int& cX, int& cY);
